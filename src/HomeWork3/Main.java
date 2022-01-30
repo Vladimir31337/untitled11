8c063a68d1a17bb809dd1a438f5b08c0b4311b8b
Vladimir31337 <otmopo3ok-bobka@yandex.ru>
2022-01-30 21:35:35 +0300

    Урок №4

--- src/HomeWork3/Main.java
+++ src/HomeWork3/Main.java
@@ -1,4 +1,23 @@
 package HomeWork3;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Apple app1 = new Apple();
+        Orange orange1 = new Orange();
+        System.out.println(app1.getWeight());
+        System.out.println(orange1.getWeight());
+
+
+        FruitBox<Apple> appleBox = new FruitBox<Apple>(app1, 3);
+        System.out.println(appleBox.getWeight());
+        FruitBox<Orange> orangeBox = new FruitBox<Orange>(orange1, 2);
+        System.out.println(orangeBox.getWeight());
+
+        System.out.println();
+        System.out.println(orangeBox.compare(appleBox));
+
+    }
 }
+
